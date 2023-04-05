b1fcdee3cd05568a4de050adf4d1ccbf8fef84e1
PhoebeTracyChaoKazungu <kazungutracy711@gmail.com>
2023-04-06 00:16:44 +0300

    12th commit

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ fetch('https://fakestoreapi.com/products').then ((data)=>{
 return data.json();
 }).then((completedata)=>{
 // console.log(completedata);
+
+let data1="";
+completedata.map((values)=>{
+data1+=` <div class="card">
